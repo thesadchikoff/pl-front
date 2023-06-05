5a6c624c9d1a6a4816130baeca82bb9bcf9573e6
bombasters <59049797+bombasters@users.noreply.github.com>
2023-06-05 23:08:06 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = `http://localhost:8080/`
+export const API_URL = `https://7eca-80-80-197-180.eu.ngrok.io`
 
 const $api = axios.create({
 	baseURL: API_URL,
